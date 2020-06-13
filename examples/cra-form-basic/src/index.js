58982bfd5de9e4c7ccaf798be8e786aee74ec978
Olivier Franki <olivier.franki@gmail.com>
2020-06-13 21:22:02 +0200

    add eslint cra

---
+++ examples/cra-form-basic/src/index.js
@@ -0,0 +1,16 @@
+import { App } from "onekijs-cra";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch } from "react-router-dom";
+import { IndexPage } from "./pages";
+
+ReactDOM.render(
+  <App>
+    <Switch>
+      <Route>
+        <IndexPage />
+      </Route>
+    </Switch>
+  </App>,
+  document.getElementById("root")
+);
