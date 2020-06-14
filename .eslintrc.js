8c2be51340aa093a1a1455527ace3d2b83405169
Bruno Franki <bruno.franki@gmail.com>
2020-06-14 09:40:26 +0200

    configure eslint to add prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'react-app',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "react-app"
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+      },
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  },
 };
