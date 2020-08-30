c793f1913ce411bc7a39537dfdf7a504fa380ec0
Bruno Franki <bruno.franki@gmail.com>
2020-08-30 09:41:56 +0200

    rename package and version

--- examples/cra-app-basic/src/index.tsx
+++ examples/cra-app-basic/src/index.tsx
@@ -1,4 +1,4 @@
-import { App } from 'onekijs-cra';
+import { App } from 'onekijs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link, Route, Switch } from 'react-router-dom';
