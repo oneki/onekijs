10a7b22b5a7d6c30a6e64e06aa5bc4f9e778e2b5
Bruno Franki <bruno.franki@gmail.com>
2020-11-04 08:01:07 +0100

    backup

---
+++ getting-started/step3-global-state/src/cart/index.tsx
@@ -0,0 +1,13 @@
+import { useGlobalSelector } from 'onekijs';
+import React, { FC } from 'react';
+import { ProductType } from '../products/@components/Product';
+import Cart from './@components/Cart';
+
+const CartPage: FC = () => {
+  // retrieve the content of the cart from the global store of the application
+  // Each time the cart is updated, the component is refreshed
+  const cart: ProductType[] = useGlobalSelector('cart', []); // TODO change to useGlobalProp
+  return <Cart cart={cart} />;
+};
+
+export default CartPage;
