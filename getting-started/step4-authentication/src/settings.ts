7842f16dbd753d5802dee0f9c91c9125a21bba55
Bruno Franki <bruno.franki@gmail.com>
2020-11-04 23:27:42 +0100

    switch to http

--- getting-started/step4-authentication/src/settings.ts
+++ getting-started/step4-authentication/src/settings.ts
@@ -1,6 +1,9 @@
 import { AppSettings } from 'onekijs';
 
 export default {
+  server: {
+    baseUrl: 'http://code.oneki.org:4000',
+  },
   routes: {
     // redirect to /auth if non authenticated user
     // tries to access a secured page
