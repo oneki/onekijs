ab6d1b82323c76063e0827555278a20e1318c3c6
Bruno Franki <bruno.franki@gmail.com>
2021-08-07 11:05:05 +0200

    chore: add getting started next step06

---
+++ getting-started/next/step06-notification/src/modules/core/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { useSecurityContext } from 'onekijs';
+import React from 'react';
+import { Link } from 'onekijs';
+
+const Navbar: React.FC = () => {
+  const [loggedUser] = useSecurityContext('username');
+  return (
+    <div className="app-top-bar">
+      <Link href="/">
+        <h1>My Store</h1>
+      </Link>
+      <div className="app-top-bar-right">
+        {loggedUser && (
+          <div className="user">
+            {loggedUser}{' '}
+            <Link className="logout" href="/logout">
+              [Log out]
+            </Link>
+          </div>
+        )}
+        <Link href="/cart" className="button fancy-button">
+          <i className="material-icons">shopping_cart</i>
+          Checkout
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- getting-started/next/step06-notification/src/modules/core/layouts/AppLayout.tsx
+++ getting-started/next/step06-notification/src/modules/core/layouts/AppLayout.tsx
@@ -1,10 +1,12 @@
-import { layout } from 'onekijs';
 import React, { FC } from 'react';
+import { layout } from 'onekijs';
 import Navbar from '../components/Navbar';
+import NotificationCenter from '../components/NotificationCenter';
 
 const AppLayout: FC = ({ children }) => {
   return (
     <div>
+      <NotificationCenter />
       <Navbar />
       <div className="container">{children}</div>
     </div>
