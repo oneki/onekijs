0dbda0ed35c23a559635e53cc58025e9fc1c248c
Bruno Franki <bruno.franki@gmail.com>
2021-08-09 16:10:27 +0200

    chore: remove onekijs-cra and onekijs-next

--- packages/onekijs/next/src/index.ts
+++
@@ -1,6 +0,0 @@
-import 'regenerator-runtime/runtime.js';
-export { default as withNotFound } from './lib/404';
-export { App as NextApp } from './lib/app';
-export { default as NextRouter } from './lib/router/NextRouter';
-export { default as useParams } from './lib/router/useParams';
-export { AppProps as NextAppProps } from './lib/typings';
--- packages/onekijs/next/src/lib/typings.ts
+++
@@ -1,4 +0,0 @@
-import { AppProps as NextAppProps } from 'next/app';
-import { CoreAppProps } from 'onekijs';
-
-export interface AppProps extends CoreAppProps, NextAppProps {}
