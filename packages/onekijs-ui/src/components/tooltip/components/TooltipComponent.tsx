4762fbd239525c42fcf13b789d1efbd5c5f9db3a
Olivier Franki <olivier.franki@smals.be>
2023-07-11 22:40:31 +0200

    do not print two tooltip at the same time

--- packages/onekijs-ui/src/components/tooltip/components/TooltipComponent.tsx
+++ packages/onekijs-ui/src/components/tooltip/components/TooltipComponent.tsx
@@ -1,11 +1,12 @@
 import { FCC } from 'onekijs-framework';
-import React from 'react';
+import React, { useEffect, useId, useRef, useState } from 'react';
 import { usePopperTooltip } from 'react-popper-tooltip';
 import { addClassname } from '../../../utils/style';
 import { TooltipProps } from '../typings';
 
 const TooltipComponent: FCC<TooltipProps> = (props) => {
   const { content, className, popperOptions, children, delayHide = 300, ...tooltipConfig } = props;
+  const [show, setShow] = useState<boolean>(true);
   const tooltipOptions = Object.assign(
     {
       interactive: true,
@@ -21,11 +22,32 @@ const TooltipComponent: FCC<TooltipProps> = (props) => {
   const { style: containerStyle, ...containerProps } = getTooltipProps({ className: 'o-tooltip-container' });
   const { style: arrowStyle, ...arrowProps } = getArrowProps({ className: 'o-tooltip-arrow' });
   const classNames = addClassname('o-tooltip', className);
+  const uid = useId();
+
+  useEffect(() => {
+    if (visible) {
+      const event = new CustomEvent<string>('tooltipVisible', {
+        detail: uid,
+      });
+      document.dispatchEvent(event);
+    }
+  }, [visible, uid]);
+
+  useEffect(() => {
+    const listener = (e: any) => {
+      setShow(e.detail === uid);
+    };
+    document.addEventListener('tooltipVisible', listener);
+
+    return () => {
+      document.removeEventListener('tooltipVisible', listener);
+    };
+  }, [uid]);
 
   return (
     <div className={classNames} ref={setTriggerRef}>
       {children}
-      {visible && content && (
+      {show && visible && content && (
         <div ref={setTooltipRef} style={containerStyle as React.CSSProperties} {...containerProps}>
           <div style={arrowStyle as React.CSSProperties} {...arrowProps} />
           {content}
