7f7f7844d1b9276594ece11030dca4d5e2e91508
Bruno Franki <bruno.franki@gmail.com>
2023-08-13 19:39:43 +0200

    chore: update example (google login page)

--- examples/cra-examples/src/auth/login/GoogleLoginPage.tsx
+++ examples/cra-examples/src/auth/login/GoogleLoginPage.tsx
@@ -2,27 +2,18 @@ import { useLogin } from 'onekijs';
 import React from 'react';
 
 const GoogleLoginPage: React.FC = () => {
-  // useLogin provides a 'submit' method that sends the credentials
-  // to a backend server. The URL/path of the backend server is
-  // configured in src/settings.ts like this
+  // useLogin redirects the user to Google so he can enter the username / password
+  // src/settings.ts looks like this
+  //
   // const settings = {
   //   ...
   //   idp: {
   //     google: {
-  //       // Standard authorization code flow (authorization code exchanged by the backend server)
-  //       type: 'oidc_server', 
-
-  //       // client id given by Google (from .env.local file)
-  //       clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID, 
-
-  //       // useLogin redirects to this URL so the user can enter the username / password
-  //       authorizeEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-  //       tokenEndpoint: '/api/oauth2/google/token',
-
-  //       // URL for retrieving logged-in user data
-  //       userinfoEndpoint: 'https://accounts.google.com/.well-known/openid-configuration?scope=openid%20profile%20email', 
-  //       logoutEndpoint: '/api/oauth2/google/logout',
+  //       type: 'oidc_server',  // Standard authorization code flow (authorization code exchanged by the backend server)
+  //       clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID, // client id given by Google (from .env.local file)
+  //       authorizeEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth', // useLogin redirects to this URL so the user can enter the username / password
   //       scope: 'openid email profile',
+  //       ...
   //     },
   //   }
   // }
