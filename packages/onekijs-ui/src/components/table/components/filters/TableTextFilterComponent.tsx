747a0d53310d8135bdb87cebefa37a4ca43c314d
Olivier Franki <olivier.franki@smals.be>
2024-02-12 20:07:40 +0100

    set that default filter for a text column is i_like

--- packages/onekijs-ui/src/components/table/components/filters/TableTextFilterComponent.tsx
+++ packages/onekijs-ui/src/components/table/components/filters/TableTextFilterComponent.tsx
@@ -3,11 +3,11 @@ import { TableFilterOperator, TableHeaderCellProps } from '../../typings';
 import TableInputFilterComponent from './TableInputFilterComponent';
 
 const operators: TableFilterOperator[] = [
-  { text: 'contains', operator: 'like', not: false },
+  { text: 'contains', operator: 'i_like', not: false },
   { text: 'ends with', operator: 'ew', not: false },
   { text: 'equals', operator: 'eq', not: false },
   { text: 'starts with', operator: 'sw', not: false },
-  { text: 'does not contain', operator: 'like', not: true },
+  { text: 'does not contain', operator: 'i_like', not: true },
   { text: 'does not end with', operator: 'ew', not: true },
   { text: 'is not equal', operator: 'eq', not: true },
   { text: 'does not start with', operator: 'sw', not: true },
