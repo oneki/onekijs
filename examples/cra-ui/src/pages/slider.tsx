ab6e79213e61eeb47f4c1f33a2970f02bc6dd3a5
Olivier Franki <olivier.franki@smals.be>
2025-08-02 16:40:49 +0200

    add slider example

---
+++ examples/cra-ui/src/pages/slider.tsx
@@ -0,0 +1,17 @@
+import { Tabs, Tab, ComponentStyle, Slider } from 'onekijs-ui';
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const sliderStyle: ComponentStyle<{}> = () => {
+  return css``;
+};
+
+const Page: React.FC<{ className?: string }> = ({ className }) => {
+  return (
+    null
+  );
+};
+
+export const SliderPage = styled(Page)`
+  ${sliderStyle}
+`;
